ab0361f42d9f75e7f1e4dc22d59eef025f9dc3da
Ogooezeani <ogooezeani@gmail.com>
2023-03-15 19:29:46 +0100

    Refactored code and added a link to go to home

--- javascript/index.js
+++ javascript/index.js
@@ -1,25 +1,31 @@
-function refreshTime() {
-  let molndalElement = document.querySelector("#molndal");
-  if (molndalElement) {
-    let molndalDateElement = molndalElement.querySelector(".date");
-    let molndalTimeElement = molndalElement.querySelector(".time");
-    let molndalTime = moment().tz("Europe/Stockholm");
-    molndalDateElement.innerHTML = molndalTime.format("MMMM Do YYYY");
-    molndalTimeElement.innerHTML = molndalTime.format(
-      "h:mm:ss:SSS [<small>]A[</small>]"
-    );
-  }
+function showElement(elementId) {
+  let element = document.querySelector(elementId);
+  element.removeAttribute("hidden");
+  return element;
+}
 
-  let lagosElement = document.querySelector("#lagos");
-  if (lagosElement) {
-    let lagosDateElement = lagosElement.querySelector(".date");
-    let lagosTimeElement = lagosElement.querySelector(".time");
-    let lagosTime = moment().tz("Africa/Lagos");
-    lagosDateElement.innerHTML = lagosTime.format("MMMM Do YYYY");
-    lagosTimeElement.innerHTML = lagosTime.format(
+function hideElement(elementId) {
+  let element = document.querySelector(elementId);
+  element.setAttribute("hidden", "hidden");
+  return element;
+}
+
+function updateElement(cityId, timezone) {
+  let cityElement = document.querySelector(cityId);
+  if (cityElement) {
+    let cityTime = moment().tz(timezone);
+    cityElement.querySelector(".date").innerHTML =
+      cityTime.format("MMMM Do YYYY");
+    cityElement.querySelector(".time").innerHTML = cityTime.format(
       "h:mm:ss:SSS [<small>]A[</small>]"
     );
   }
+}
+
+function refreshTime() {
+  updateElement("#molndal", "Europe/Stockholm");
+
+  updateElement("#lagos", "Africa/Lagos");
 }
 
 function updateLocation(event) {
@@ -33,8 +39,10 @@ function updateLocation(event) {
   }
   let locationName = locationTimeZone.replace("_", " ").split("/")[1];
   let locationTime = moment().tz(locationTimeZone);
-  let citiesElement = document.querySelector("#cities");
-  citiesElement.innerHTML = `<div class ="city">
+  hideElement("#cities");
+  showElement("#goToHome");
+  let selectedCityElement = showElement("#selectedCity");
+  selectedCityElement.innerHTML = `<div class ="city">
     <div>
     <h2>${locationName}</h2>
     <div class="date">${locationTime.format("MM Do YYYY")}</div>
@@ -46,8 +54,16 @@ function updateLocation(event) {
     `;
 }
 
+function showAllCities(event) {
+  hideElement("#selectedCity");
+  hideElement("#goToHome");
+  showElement("#cities");
+}
+
 refreshTime();
 setInterval(refreshTime, 1000);
 
 let locationSelect = document.querySelector("#location");
 locationSelect.addEventListener("change", updateLocation);
+
+document.querySelector("#goToHome").addEventListener("click", showAllCities);
